4a824cf82f2301644e8621227ea9610907750f7d
Jorge <jcruzagui@gmail.com>
2022-12-15 13:40:43 -0500

    Initial Commit

---
+++ mcSwiftUI/mcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcSwiftUIApp.swift
+//  mcSwiftUI
+//
+//  Created by Jcruz on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct mcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
